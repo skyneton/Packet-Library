97c7f63f31a2d9fe1b4485af6edd76e4a40e12ac
skyneton <67633420+skyneton@users.noreply.github.com>
2021-06-07 11:47:03 +0900

    UPLOAD

---
+++ SocketPacket/SocketPacket/Network/Packet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SocketPacket.Network {
+    [Serializable]
+    public class Packet {
+        public int type = 0;
+        public Packet() {
+            type = 0;
+        }
+        public static byte[] Serialize(Packet packet) {
+            MemoryStream ms = new MemoryStream();
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Binder = new PacketBinder();
+
+            bf.Serialize(ms, packet);
+
+            byte[] result = ms.ToArray();
+            ms.Dispose();
+
+            return result;
+        }
+
+        public static Packet Deserialize(byte[] data) {
+            MemoryStream ms = new MemoryStream(data);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Binder = new PacketBinder();
+
+            Packet result = bf.Deserialize(ms) as Packet;
+
+            ms.Dispose();
+            return result;
+        }
+    }
+}
